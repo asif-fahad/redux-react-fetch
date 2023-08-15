92f2d09e7dadd980c8bc8db853641b209068cead
asif-fahad <asif123fahad@gmail.com>
2023-08-15 22:45:32 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Todos from './components/Todos'
+
+function App() {
+
+  return (
+    <>
+      <h1>Welcome to React Redux</h1>
+      <Todos></Todos>
+    </>
+  )
+}
+
+export default App
